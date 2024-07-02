b28356a9f526f29a56d5742f784651b969ab0fb8
Aam Hermansyah <amzhermanzyah@gmail.com>
2024-07-02 21:03:36 +0700

    [ADJUST] Rename file certification.tsx

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,12 +1,9 @@
 "use client"
 
-import { Card } from "@/components/ui/card"
-import CardSertification from "./_components/sertifications"
+import CardSertification from "./_components/certifications"
 import ProfileSection from "./_components/profile-section"
 
-
 export default function ProfilePage() {
-
   return (
     <div className="w-full grid grid-cols-12 gap-4">
       <div className="col-span-12 md:col-span-5">
