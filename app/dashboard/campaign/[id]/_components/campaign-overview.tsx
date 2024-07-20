f589d648f4e0b35b075312c769d7c4c9bb129353
EgaAprianto <egaaprianto04@gmail.com>
2024-07-20 13:13:43 +0700

    [UPDATE] - Detail Image with overflow-x-auto

--- app/dashboard/campaign/[id]/_components/campaign-overview.tsx
+++ app/dashboard/campaign/[id]/_components/campaign-overview.tsx
@@ -1,43 +1,38 @@
-'use client'
-import React, { useState } from 'react'
-import { Progress } from '../../../../../components/ui/progress'
-import { cn, formatIndonesianDate, formatRupiah } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
-import { IoMdShare } from 'react-icons/io'
-import { Badge } from '@/components/ui/badge'
-import { Campaign, User, UserRole } from '@prisma/client'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+"use client";
+import React, { useState } from "react";
+import { Progress } from "../../../../../components/ui/progress";
+import { cn, formatIndonesianDate, formatRupiah } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { IoMdShare } from "react-icons/io";
+import { Badge } from "@/components/ui/badge";
+import { Campaign, User, UserRole } from "@prisma/client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface IProps {
   className?: string;
-  data: Omit<Campaign, 'description'> & {
-    creator?: User
+  data: Omit<Campaign, "description"> & {
+    creator?: User;
   };
   role: UserRole | null;
 }
 
 function CampaignOverview({ className, data, role }: IProps) {
-  const navigate = useRouter()
+  const navigate = useRouter();
 
   const detailImages = [
     data.image,
-    'https://fastly.picsum.photos/id/83/1600/900.jpg?hmac=R9BQdLAPaGw27suOHRBe6G6xb7m1XzQlbsx7as1N7-s',
-    'https://fastly.picsum.photos/id/83/1600/900.jpg?hmac=R9BQdLAPaGw27suOHRBe6G6xb7m1XzQlbsx7as1N7-s',
-    'https://fastly.picsum.photos/id/83/1600/900.jpg?hmac=R9BQdLAPaGw27suOHRBe6G6xb7m1XzQlbsx7as1N7-s',
-    'https://fastly.picsum.photos/id/83/1600/900.jpg?hmac=R9BQdLAPaGw27suOHRBe6G6xb7m1XzQlbsx7as1N7-s',
+    "https://fastly.picsum.photos/id/83/1600/900.jpg?hmac=R9BQdLAPaGw27suOHRBe6G6xb7m1XzQlbsx7as1N7-s",
+    "https://fastly.picsum.photos/id/83/1600/900.jpg?hmac=R9BQdLAPaGw27suOHRBe6G6xb7m1XzQlbsx7as1N7-s",
+    "https://fastly.picsum.photos/id/83/1600/900.jpg?hmac=R9BQdLAPaGw27suOHRBe6G6xb7m1XzQlbsx7as1N7-s",
+    "https://fastly.picsum.photos/id/83/1600/900.jpg?hmac=R9BQdLAPaGw27suOHRBe6G6xb7m1XzQlbsx7as1N7-s",
   ];
 
   const [mainImage, setMainImage] = useState(data.image);
 
   return (
     <>
-      <div
-        className={cn(
-          "bg-background rounded-md shadow-sm p-4",
-          className
-        )}
-      >
+      <div className={cn("bg-background rounded-md shadow-sm p-4", className)}>
         <div className="w-full flex flex-col sm:flex-row justify-between gap-x-2 sm:items-end mb-2 sm:mb-1">
           {data.creator && (
             <span className="block text-xs sm:text-sm">
@@ -45,42 +40,36 @@ function CampaignOverview({ className, data, role }: IProps) {
             </span>
           )}
           <span className="block text-xs sm:text-sm">
-            Publikasi: <time className="italic">{formatIndonesianDate(data.createdAt)}</time>
+            Publikasi:{" "}
+            <time className="italic">
+              {formatIndonesianDate(data.createdAt)}
+            </time>
           </span>
         </div>
         <div className="grid grid-cols-2 gap-x-2 sm:gap-x-4 gap-y-2 sm:gap-y-4 ">
-        <div className='flex flex-col'>
-            <div className="relative w-full aspect-[4/3] rounded-md overflow-hidden">
-              <img
-                src={mainImage}
-                className="w-full h-full object-cover"
-                alt="campaign banner"
-              />
-              <div className="absolute left-2 top-2">
-                <Badge
-                  variant={
-                    data.status === 'RUNNING' ? 'info' : data.status === 'CLOSED' ? 'destructive' : 'success'
-                  }
-                  className="text-[10px] sm:text-xs"
-                >
-                  {data.status === 'RUNNING' ? 'Berjalan' : data.status === 'CLOSED' ? 'Ditutup' : 'Selesai'}
-                </Badge>
-              </div>
-            </div>
-            {/* Horizontal Scrollable Images Section */}
-            <div className="col-span-2 mt-4">
-              <div className="overflow-x-auto flex space-x-2">
-                {detailImages.map((image, index) => (
-                  <div key={index} className="relative min-w-[33%] sm:min-w-[25%] aspect-[4/3] rounded-md overflow-hidden">
-                    <img
-                      src={image}
-                      className="w-full h-full object-cover cursor-pointer"
-                      alt={`Detail Image ${index + 1}`}
-                      onClick={() => setMainImage(image)}
-                    />
-                  </div>
-                ))}
-              </div>
+          <div className="relative w-full aspect-[4/3] rounded-md overflow-hidden">
+            <img
+              src={mainImage}
+              className="w-full h-full object-cover"
+              alt="campaign banner"
+            />
+            <div className="absolute left-2 top-2">
+              <Badge
+                variant={
+                  data.status === "RUNNING"
+                    ? "info"
+                    : data.status === "CLOSED"
+                    ? "destructive"
+                    : "success"
+                }
+                className="text-[10px] sm:text-xs"
+              >
+                {data.status === "RUNNING"
+                  ? "Berjalan"
+                  : data.status === "CLOSED"
+                  ? "Ditutup"
+                  : "Selesai"}
+              </Badge>
             </div>
           </div>
 
@@ -94,13 +83,17 @@ function CampaignOverview({ className, data, role }: IProps) {
               </span>
               <div className="flex flex-row justify-between text-gray-700 mt-1 sm:mt-2">
                 <div>
-                  <h4 className="text-xs sm:text-base leading-3 text-gray-500">Kategori</h4>
+                  <h4 className="text-xs sm:text-base leading-3 text-gray-500">
+                    Kategori
+                  </h4>
                   <span className="block font-semibold text-sm sm:text-lg text-secondary capitalize">
                     {data.category}
                   </span>
                 </div>
                 <div className="text-right">
-                  <h4 className="text-xs sm:text-base leading-3 text-gray-500">Wakif</h4>
+                  <h4 className="text-xs sm:text-base leading-3 text-gray-500">
+                    Wakif
+                  </h4>
                   <span className="block font-semibold text-sm sm:text-lg">
                     {data.numberOfWakif}
                   </span>
@@ -111,12 +104,12 @@ function CampaignOverview({ className, data, role }: IProps) {
               <Button variant="outline">
                 <IoMdShare className="text-base" />
               </Button>
-              {role === 'USER' && data.status === 'RUNNING' ? (
-                <Link href={`/dashboard/berwakaf?campaign_id=${data.id}`} className="inline-block flex-1">
-                  <Button
-                    variant="secondary"
-                    className="relative w-full"
-                  >
+              {role === "USER" && data.status === "RUNNING" ? (
+                <Link
+                  href={`/dashboard/berwakaf?campaign_id=${data.id}`}
+                  className="inline-block flex-1"
+                >
+                  <Button variant="secondary" className="relative w-full">
                     Donasi sekarang
                     <span className="absolute -top-2 -right-2 bg-sky-200 text-secondary rounded-full w-5 h-5 flex items-center justify-center text-xs font-bold">
                       💰
@@ -130,7 +123,9 @@ function CampaignOverview({ className, data, role }: IProps) {
                   disabled={!!role}
                   onClick={() => {
                     if (!role) {
-                      const callbackUrl = encodeURIComponent(`/dashboard/berwakaf?campaign_id=${data.id}`);
+                      const callbackUrl = encodeURIComponent(
+                        `/dashboard/berwakaf?campaign_id=${data.id}`
+                      );
                       navigate.push(`/auth/login?callbackUrl=${callbackUrl}`);
                     }
                   }}
@@ -143,10 +138,27 @@ function CampaignOverview({ className, data, role }: IProps) {
               )}
             </div>
           </div>
+          {/* Horizontal Scrollable Images Section */}
+            <div className="overflow-x-auto flex space-x-2">
+              {detailImages.map((image, index) => (
+                <div
+                  key={index}
+                  className="relative min-w-[33%] sm:min-w-[25%] aspect-[4/3] rounded-md overflow-hidden"
+                >
+                  <img
+                    src={image}
+                    className="w-full h-full object-cover cursor-pointer"
+                    alt={`Detail Image ${index + 1}`}
+                    onClick={() => setMainImage(image)}
+                  />
+                </div>
+              ))}
+          </div>
           <div className="col-span-2">
-            {data.status !== 'RUNNING' && (
+            {data.status !== "RUNNING" && (
               <p className="text-xs p-2 bg-destructive/10 text-destructive rounded">
-                Kampanye yang sudah selesai atau ditutup tidak bisa berdonasi. Silahkan pilih kampanye lain:)
+                Kampanye yang sudah selesai atau ditutup tidak bisa berdonasi.
+                Silahkan pilih kampanye lain:)
               </p>
             )}
             <div className="flex flex-row justify-between mt-2">
@@ -164,19 +176,24 @@ function CampaignOverview({ className, data, role }: IProps) {
               </div>
             </div>
             <div className="sm:mt-2">
-              <span className="text-xs">Total: {formatRupiah(data.target)}</span>
-              <Progress value={(data.collected / (data.target)) * 100} className="h-1.5 sm:h-2" />
+              <span className="text-xs">
+                Total: {formatRupiah(data.target)}
+              </span>
+              <Progress
+                value={(data.collected / data.target) * 100}
+                className="h-1.5 sm:h-2"
+              />
             </div>
             <div className="flex sm:hidden gap-2 mt-4">
               <Button size="sm" variant="outline">
                 <IoMdShare className="text-xs" />
               </Button>
-              {role === 'USER' && data.status === 'RUNNING' ? (
-                <Link href={`/dashboard/berwakaf?campaign_id=${data.id}`} className="inline-block flex-1">
-                  <Button
-                    variant="secondary"
-                    className="relative w-full"
-                  >
+              {role === "USER" && data.status === "RUNNING" ? (
+                <Link
+                  href={`/dashboard/berwakaf?campaign_id=${data.id}`}
+                  className="inline-block flex-1"
+                >
+                  <Button variant="secondary" className="relative w-full">
                     Donasi sekarang
                     <span className="absolute -top-2 -right-2 bg-sky-200 text-secondary rounded-full w-5 h-5 flex items-center justify-center text-xs font-bold">
                       💰
@@ -190,7 +207,9 @@ function CampaignOverview({ className, data, role }: IProps) {
                   disabled={!!role}
                   onClick={() => {
                     if (!role) {
-                      const callbackUrl = encodeURIComponent(`/dashboard/berwakaf?campaign_id=${data.id}`);
+                      const callbackUrl = encodeURIComponent(
+                        `/dashboard/berwakaf?campaign_id=${data.id}`
+                      );
                       navigate.push(`/auth/login?callbackUrl=${callbackUrl}`);
                     }
                   }}
@@ -205,7 +224,12 @@ function CampaignOverview({ className, data, role }: IProps) {
           </div>
           <div className="col-span-2 p-4 rounded-md bg-muted">
             <p className="text-xs">
-              <b>Disclaimer:</b> Kampanye ini diadakan secara resmi oleh pengurus dari Pondok Pesantren Mahasiswa Al-Ihsan dan menjadi tanggung jawabnya. Jika ada masalah atau kecurigaan silahkan <a href="#" className="text-secondary underline">laporkan kepada pihak pengembang.</a>
+              <b>Disclaimer:</b> Kampanye ini diadakan secara resmi oleh
+              pengurus dari Pondok Pesantren Mahasiswa Al-Ihsan dan menjadi
+              tanggung jawabnya. Jika ada masalah atau kecurigaan silahkan{" "}
+              <a href="#" className="text-secondary underline">
+                laporkan kepada pihak pengembang.
+              </a>
             </p>
           </div>
         </div>
