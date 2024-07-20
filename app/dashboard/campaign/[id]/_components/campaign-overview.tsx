f02344b62af2d8989e793219a1fa523d574d4c62
pandupangestu <pangestup60@gmail.com>
2024-07-20 23:27:35 +0700

    [UPDATE] - Hide Scrollbar & Change Layout

--- app/dashboard/campaign/[id]/_components/campaign-overview.tsx
+++ app/dashboard/campaign/[id]/_components/campaign-overview.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import React, { useState } from "react";
 import { Progress } from "../../../../../components/ui/progress";
@@ -46,7 +47,8 @@ function CampaignOverview({ className, data, role }: IProps) {
             </time>
           </span>
         </div>
-        <div className="grid grid-cols-2 gap-x-2 sm:gap-x-4 gap-y-2 sm:gap-y-4 ">
+
+        <div className="grid grid-cols-2 gap-x-2 sm:gap-x-4 gap-y-2 sm:gap-y-4">
           <div className="flex flex-col">
             <div className="relative w-full aspect-[4/3] rounded-md overflow-hidden">
               <img
@@ -73,8 +75,9 @@ function CampaignOverview({ className, data, role }: IProps) {
                 </Badge>
               </div>
             </div>
+
             {/* Horizontal Scrollable Images Section */}
-            <div className="overflow-x-auto flex space-x-2 mt-2">
+            <div className="overflow-x-auto flex space-x-2 mt-2 no-scrollbar">
               {detailImages.map((image, index) => (
                 <div
                   key={index}
@@ -90,9 +93,10 @@ function CampaignOverview({ className, data, role }: IProps) {
               ))}
             </div>
           </div>
+
           <div className="sm:space-y-2 flex flex-col justify-between">
-            <div>
-              <h2 className="text-sm sm:text-lg font-semibold leading-tight">
+            <div className="space-y-10">
+              <h2 className="text-sm sm:text-lg font-semibold leading-tight mb-4">
                 {data.title}
               </h2>
               <span className="text-sm sm:text-xl text-secondary font-bold tracking-wide">
@@ -116,43 +120,43 @@ function CampaignOverview({ className, data, role }: IProps) {
                   </span>
                 </div>
               </div>
-            </div>
-            <div className="hidden sm:flex gap-2 sm:mt-6">
-              <Button variant="outline">
-                <IoMdShare className="text-base" />
-              </Button>
-              {role === "USER" && data.status === "RUNNING" ? (
-                <Link
-                  href={`/dashboard/berwakaf?campaign_id=${data.id}`}
-                  className="inline-block flex-1"
-                >
-                  <Button variant="secondary" className="relative w-full">
+              <div className="hidden sm:flex gap-2">
+                <Button variant="outline">
+                  <IoMdShare className="text-base" />
+                </Button>
+                {role === "USER" && data.status === "RUNNING" ? (
+                  <Link
+                    href={`/dashboard/berwakaf?campaign_id=${data.id}`}
+                    className="inline-block flex-1"
+                  >
+                    <Button variant="secondary" className="relative w-full">
+                      Donasi sekarang
+                      <span className="absolute -top-2 -right-2 bg-sky-200 text-secondary rounded-full w-5 h-5 flex items-center justify-center text-xs font-bold">
+                        💰
+                      </span>
+                    </Button>
+                  </Link>
+                ) : (
+                  <Button
+                    variant="secondary"
+                    className="relative flex-1"
+                    disabled={!!role}
+                    onClick={() => {
+                      if (!role) {
+                        const callbackUrl = encodeURIComponent(
+                          `/dashboard/berwakaf?campaign_id=${data.id}`
+                        );
+                        navigate.push(`/auth/login?callbackUrl=${callbackUrl}`);
+                      }
+                    }}
+                  >
                     Donasi sekarang
                     <span className="absolute -top-2 -right-2 bg-sky-200 text-secondary rounded-full w-5 h-5 flex items-center justify-center text-xs font-bold">
                       💰
                     </span>
                   </Button>
-                </Link>
-              ) : (
-                <Button
-                  variant="secondary"
-                  className="relative flex-1"
-                  disabled={!!role}
-                  onClick={() => {
-                    if (!role) {
-                      const callbackUrl = encodeURIComponent(
-                        `/dashboard/berwakaf?campaign_id=${data.id}`
-                      );
-                      navigate.push(`/auth/login?callbackUrl=${callbackUrl}`);
-                    }
-                  }}
-                >
-                  Donasi sekarang
-                  <span className="absolute -top-2 -right-2 bg-sky-200 text-secondary rounded-full w-5 h-5 flex items-center justify-center text-xs font-bold">
-                    💰
-                  </span>
-                </Button>
-              )}
+                )}
+              </div>
             </div>
           </div>
 
@@ -197,7 +201,7 @@ function CampaignOverview({ className, data, role }: IProps) {
                 >
                   <Button variant="secondary" className="relative w-full">
                     Donasi sekarang
-                    <span className="absolute -top-2 -right-2 bg-sky-200 text-secondary rounded-full w-5 h-5 flex items-center justify-center text-xs font-bold">
+                    <span className="absolute -top-1.5 -right-1.5 bg-sky-200 text-secondary rounded-full w-4 h-4 flex items-center justify-center text-[10px] font-bold">
                       💰
                     </span>
                   </Button>
@@ -217,22 +221,12 @@ function CampaignOverview({ className, data, role }: IProps) {
                   }}
                 >
                   Donasi sekarang
-                  <span className="absolute -top-2 -right-2 bg-sky-200 text-secondary rounded-full w-5 h-5 flex items-center justify-center text-xs font-bold">
+                  <span className="absolute -top-1.5 -right-1.5 bg-sky-200 text-secondary rounded-full w-4 h-4 flex items-center justify-center text-[10px] font-bold">
                     💰
                   </span>
                 </Button>
               )}
             </div>
-          </div>
-          <div className="col-span-2 p-4 rounded-md bg-muted">
-            <p className="text-xs">
-              <b>Disclaimer:</b> Kampanye ini diadakan secara resmi oleh
-              pengurus dari Pondok Pesantren Mahasiswa Al-Ihsan dan menjadi
-              tanggung jawabnya. Jika ada masalah atau kecurigaan silahkan{" "}
-              <a href="#" className="text-secondary underline">
-                laporkan kepada pihak pengembang.
-              </a>
-            </p>
           </div>
         </div>
       </div>
